4932b883c156c068e77e7d7607e197396bb083b4
Mico <2114678406@qq.com>
2024-09-23 14:00:24 +0800

    add World.cpp

---
+++ World.cpp
@@ -0,0 +1,17 @@
+#include "World.h"
+
+bool World::readIsAvailable(Point& point)
+{
+    // int x = point.x;
+    // int y = point.y;
+    // if(x < 0 || x>=width || y<0 || y>=height) return false;
+    // return pointSet[x][y]->isAvailable();
+}
+
+void World::write(const Object* object, Point& point)
+{
+    // int x = point.x;
+    // int y = point.y;
+    // if(x < 0 || x>=width || y<0 || y>=height) return;
+    // pointSet[x][y] = object;
+}
