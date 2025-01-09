9261499642a34386428d611b75379315636f9632
Mico <2114678406@qq.com>
2025-01-09 12:14:25 +0800

    add main func to World

--- World.cpp
+++ World.cpp
@@ -15,3 +15,12 @@ void World::write(const Object* object, Point& point)
     // if(x < 0 || x>=width || y<0 || y>=height) return;
     // pointSet[x][y] = object;
 }
+
+int main()
+{
+    World world(10, 10);
+    Point point(0, 0);
+    world.readIsAvailable(point);
+    world.write(new Object(), point);
+    return 0;
+}
